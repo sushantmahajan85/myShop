d1c68c232022003e1aac661574aa2e9c7f36e9d4
yasharjit <yasharjitm10@gmail.com>
2021-03-18 18:13:47 +0530

    Add Services Section

--- reactShop/react-shop/src/index.js
+++ reactShop/react-shop/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Footer from "./Footer";
+import Services from "./Services";
 
 ReactDOM.render(
     <React.StrictMode>
         <Navbar />
         <Carousel />
+        <Services />
         <Footer />
     </React.StrictMode>,
     document.getElementById("root")
