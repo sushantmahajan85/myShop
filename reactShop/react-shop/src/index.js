c68c87020ccc3fe23e9658295983ee697f3fdc82
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-03-18 17:58:50 +0530

    initial commit

--- reactShop/react-shop/src/index.js
+++ reactShop/react-shop/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
+import Footer from "./footer";
 
 ReactDOM.render(
     <React.StrictMode>
         <Navbar />
         <Carousel />
+        <Footer />
     </React.StrictMode>,
     document.getElementById("root")
 );
