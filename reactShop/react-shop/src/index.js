b1229e0a8f8ca194b9b1d6bceb3139f0577895ed
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-03-18 18:29:37 +0530

    contact form

--- reactShop/react-shop/src/index.js
+++ reactShop/react-shop/src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import Navbar from "./Navbar";
 import Carousel from "./Carousel";
 import Footer from "./Footer";
+import Contact from "./Contact";
 
 ReactDOM.render(
     <React.StrictMode>
         <Navbar />
         <Carousel />
+        <Contact />
         <Footer />
     </React.StrictMode>,
     document.getElementById("root")
