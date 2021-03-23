1ab394b813a690b13fb5d8ee3bfadd50183ded63
yasharjit <yasharjitm10@gmail.com>
2021-03-23 17:50:53 +0530

    Replace text heading logo with image logo in footer

--- reactShop/react-shop/src/Footer.js
+++ reactShop/react-shop/src/Footer.js
@@ -1,143 +1,149 @@
 import "./index.css";
+import logo from "./img/core-img/logonew.png";
 import { Helmet } from "react-helmet";
 
 const Footer = () => {
-  return (
-    <div className="Footer">
-      <footer className="footer-section">
-        <div className="container">
-          <div className="footer-cta pt-5 pb-5">
-            <div className="row">
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <i className="fas fa-map-marker-alt"></i>
-                  <div className="cta-text">
-                    <h4>Find us</h4>
-                    <span>Mugrala road, Dinanagar, Punjab</span>
-                  </div>
+    return (
+        <div className="Footer">
+            <footer className="footer-section">
+                <div className="container">
+                    <div className="footer-cta pt-5 pb-5">
+                        <div className="row">
+                            <div className="col-xl-4 col-md-4 mb-30">
+                                <div className="single-cta">
+                                    <i className="fas fa-map-marker-alt"></i>
+                                    <div className="cta-text">
+                                        <h4>Find us</h4>
+                                        <span>
+                                            Mugrala road, Dinanagar, Punjab
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-md-4 mb-30">
+                                <div className="single-cta">
+                                    <i className="fas fa-phone"></i>
+                                    <div className="cta-text">
+                                        <h4>Call us</h4>
+                                        <span>+91 9815144901</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-md-4 mb-30">
+                                <div className="single-cta">
+                                    <i className="far fa-envelope-open"></i>
+                                    <div className="cta-text">
+                                        <h4>Mail us</h4>
+                                        <span>mahajan.cyber1@gmail.com</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-content pt-5 pb-5">
+                        <div className="row">
+                            <div className="col-xl-4 col-lg-4 mb-50">
+                                <div className="footer-widget">
+                                    <div className="logo">
+                                        <a href="/">
+                                            <img src={logo} />
+                                        </a>
+                                    </div>
+                                    <div className="footer-social-icon">
+                                        <span>Follow us</span>
+                                        <a href="#">
+                                            <i className="fab fa-facebook-f facebook-bg"></i>
+                                        </a>
+                                        <a href="#">
+                                            <i className="fab fa-twitter twitter-bg"></i>
+                                        </a>
+                                        <a href="#">
+                                            <i className="fab fa-google-plus-g google-bg"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                                <div className="footer-widget">
+                                    <div className="footer-widget-heading">
+                                        <h3>Useful Links</h3>
+                                    </div>
+                                    <ul>
+                                        <li>
+                                            <a href="/">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#services">Services</a>
+                                        </li>
+                                        <li>
+                                            <a href="/pricing">Pricing</a>
+                                        </li>
+                                        <li>
+                                            <a href="/#contact">Contact</a>
+                                        </li>
+                                        <li>
+                                            <a href="/about">About Us</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                                <div className="footer-widget">
+                                    <div className="footer-widget-heading">
+                                        <h3>About Us</h3>
+                                    </div>
+                                    <div className="footer-text mb-25">
+                                        <p>
+                                            Don’t miss to subscribe to our new
+                                            feeds, kindly fill the form below.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <i className="fas fa-phone"></i>
-                  <div className="cta-text">
-                    <h4>Call us</h4>
-                    <span>+91 9815144901</span>
-                  </div>
+                <div className="copyright-area">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                                <div className="copyright-text">
+                                    <p>
+                                        Copyright &copy; 2018, All Right
+                                        Reserved
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                                <div className="footer-menu">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Terms</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Privacy</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Policy</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="col-xl-4 col-md-4 mb-30">
-                <div className="single-cta">
-                  <i className="far fa-envelope-open"></i>
-                  <div className="cta-text">
-                    <h4>Mail us</h4>
-                    <span>mahajan.cyber1@gmail.com</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="footer-content pt-5 pb-5">
-            <div className="row">
-              <div className="col-xl-4 col-lg-4 mb-50">
-                <div className="footer-widget">
-                  <div className="logo">
-                    <a href="/">
-                      <h3 className="logo_mahajan">Mahajan Cyber World</h3>
-                    </a>
-                  </div>
-                  <div className="footer-social-icon">
-                    <span>Follow us</span>
-                    <a href="#">
-                      <i className="fab fa-facebook-f facebook-bg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fab fa-twitter twitter-bg"></i>
-                    </a>
-                    <a href="#">
-                      <i className="fab fa-google-plus-g google-bg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                <div className="footer-widget">
-                  <div className="footer-widget-heading">
-                    <h3>Useful Links</h3>
-                  </div>
-                  <ul>
-                    <li>
-                      <a href="/">Home</a>
-                    </li>
-                    <li>
-                      <a href="/#services">Services</a>
-                    </li>
-                    <li>
-                      <a href="/pricing">Pricing</a>
-                    </li>
-                    <li>
-                      <a href="/#contact">Contact</a>
-                    </li>
-                    <li>
-                      <a href="/about">About Us</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                <div className="footer-widget">
-                  <div className="footer-widget-heading">
-                    <h3>About Us</h3>
-                  </div>
-                  <div className="footer-text mb-25">
-                    <p>
-                      Don’t miss to subscribe to our new feeds, kindly fill the
-                      form below.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            </footer>
+            <Helmet>
+                <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+            </Helmet>
         </div>
-        <div className="copyright-area">
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-6 col-lg-6 text-center text-lg-left">
-                <div className="copyright-text">
-                  <p>Copyright &copy; 2018, All Right Reserved</p>
-                </div>
-              </div>
-              <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-                <div className="footer-menu">
-                  <ul>
-                    <li>
-                      <a href="#">Home</a>
-                    </li>
-                    <li>
-                      <a href="#">Terms</a>
-                    </li>
-                    <li>
-                      <a href="#">Privacy</a>
-                    </li>
-                    <li>
-                      <a href="#">Policy</a>
-                    </li>
-                    <li>
-                      <a href="#">Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-      <Helmet>
-        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-      </Helmet>
-    </div>
-  );
+    );
 };
 
 export default Footer;
