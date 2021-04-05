fa65e2bb64206ad059c26495385fd78ec045b0d5
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-04-05 19:54:07 +0530

    modules ka panga along with homepage

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
                 aperiam recusandae quos dolorem minima eum in delectus ut quo
                 nam doloremque, repudiandae eligendi aut adipisci laboriosam
-                saepe fuga voluptas ipsum!
+                {/* saepe fuga voluptas ipsum! */}
             </p>
         </div>
     );
