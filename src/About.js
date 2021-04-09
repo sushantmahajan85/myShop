962b19b6474f4706cf673437a5810b90abe71d4b
yasharjit <yasharjitm10@gmail.com>
2021-04-09 23:04:01 +0530

    completed about page

--- src/About.js
+++ src/About.js
@@ -1,14 +1,26 @@
 import "./index.css";
+import img from "./img/carouselImages/carouselImg.jpg";
 
 const About = () => {
     return (
         <div className="About">
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus
-                aperiam recusandae quos dolorem minima eum in delectus ut quo
-                nam doloremque, repudiandae eligendi aut adipisci laboriosam
-                {/* saepe fuga voluptas ipsum! */}
-            </p>
+            <div className="about-image">
+                <img src={img} alt="" />
+            </div>
+            <div className="about-content">
+                <p>
+                    <strong>Mahajan Cyber World</strong> was started in 2008
+                    with only four computer systems. Within a year, the number
+                    of systems increased to ten and the cyber cafe had grown to
+                    such an extent that we had full house at all times. At
+                    Mahajan Cyber World, we deal in all kinds of computer
+                    software/hardware solutions.
+                </p>
+                <p>
+                    With the grace of God, Mahajan Cyber World is renowned in
+                    the town of Dinanagar and the nearby villages.
+                </p>
+            </div>
         </div>
     );
 };
