3db8c62deee642669a68473e034b2560df87c848
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-04-18 00:50:39 +0530

    spacing in about us

--- src/About.js
+++ src/About.js
@@ -10,13 +10,12 @@ const About = () => {
             <div className="about-content">
                 <div style={{ width: "100%", textAlign: "center" }}>
                     <p style={{ width: "70%", margin: "0 auto" }}>
-                        <strong>Mahajan Cyber World</strong>
-                        was started in 2008 with only four computer systems.
-                        Within a year, the number of systems increased to ten
-                        and the cyber cafe has grown to such an extent that we
-                        have mostly full house at all times. At Mahajan Cyber
-                        World, we deal in all kinds of computer
-                        software/hardware solutions.
+                        <strong>Mahajan Cyber World</strong> was started in 2008
+                        with only four computer systems. Within a year, the
+                        number of systems increased to ten and the cyber cafe
+                        has grown to such an extent that we have mostly full
+                        house at all times. At Mahajan Cyber World, we deal in
+                        all kinds of computer software/hardware solutions.
                         <br />
                         With the grace of God and the best wishes of our
                         customers, Mahajan Cyber World is renowned in the town
