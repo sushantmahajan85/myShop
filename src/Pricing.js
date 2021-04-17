1d6f08dcdc4c3e8c805067bede4dbb2cf0c1df41
yasharjit <yasharjitm10@gmail.com>
2021-04-17 10:05:35 +0530

    Update fontawesome version in pricing.js

--- src/Pricing.js
+++ src/Pricing.js
@@ -240,7 +240,7 @@ class Pricing extends react.Component {
                 </div>
                 <Helmet>
                     <link
-                        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"
+                        href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
                         rel="stylesheet"
                     />
                 </Helmet>
