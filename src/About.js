f4f69d1b417ae11c7999480d42f575d542a3a330
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-04-17 21:51:26 +0530

    assem bhaiya changes

--- src/About.js
+++ src/About.js
@@ -10,15 +10,17 @@ const About = () => {
             <div className="about-content">
                 <div style={{ width: "100%", textAlign: "center" }}>
                     <p style={{ width: "70%", margin: "0 auto" }}>
-                        <strong>Mahajan Cyber World</strong> was started in 2008
-                        with only four computer systems. Within a year, the
-                        number of systems increased to ten and the cyber cafe
-                        had grown to such an extent that we had full house at
-                        all times. At Mahajan Cyber World, we deal in all kinds
-                        of computer software/hardware solutions.
+                        <strong>Mahajan Cyber World</strong>
+                        was started in 2008 with only four computer systems.
+                        Within a year, the number of systems increased to ten
+                        and the cyber cafe has grown to such an extent that we
+                        have mostly full house at all times. At Mahajan Cyber
+                        World, we deal in all kinds of computer
+                        software/hardware solutions.
                         <br />
-                        With the grace of God, Mahajan Cyber World is renowned
-                        in the town of Dinanagar and the nearby villages.
+                        With the grace of God and the best wishes of our
+                        customers, Mahajan Cyber World is renowned in the town
+                        of Dinanagar and all nearby towns too.
                     </p>
                 </div>
             </div>
